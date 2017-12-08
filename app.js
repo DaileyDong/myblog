d63b26100934ae382da0df9d87af44c890884526
dongbangjun <13203878630@163.com>
2017-12-08 12:00:56 +0800

    配置数据库文件

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+//引入数据库配置文件
+var setting=require('./setting');
+//引入数据库操作实例
+var db=require('./model/db');
+//引入路由
+var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//将APP传递给路由函数使用
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +46,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//添加监听
+app.listen(8000,function () {
+    console.log("node is ok")
+})
 module.exports = app;
---
+++ setting.js
@@ -0,0 +1,8 @@
+//数据库配置信息
+module.exports={
+    cookieSecret:'dbj',//加密cookie使用的字符串
+    db:'blog',//数据库的名称
+    host:'localhost',//数据库的地址
+    port:27017//数据库的端口号
+}
+//把数据库配置信息放在这里是为了方便更改
