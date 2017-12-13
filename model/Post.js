a110f120c6e578213f5bc6959acf4bc8ce8e3627
dongbangjun <13203878630@163.com>
2017-12-13 19:18:32 +0800

    编辑和删除

--- model/Post.js
+++ model/Post.js
@@ -1,4 +1,7 @@
 var mongodb=require('./db');
+//引入markdown
+var markdown=require('markdown').markdown;
+
 function Post(name,title,content) {
     this.name=name;
     this.title=title;
@@ -19,7 +22,8 @@ Post.prototype.save=function (callback) {
         name: this.name,
         title: this.title,
         content: this.content,
-        time: now
+        time: now,
+        comments:[]//添加留言
     }
     //打开数据库
     mongodb.open(function (err, db) {
@@ -36,13 +40,13 @@ Post.prototype.save=function (callback) {
                 if (err) {
                     return callback(err);
                 }
-                callback(null, post[0]);
+                callback(null, post);
             })
         })
     })
 }
-    //读取post集合
-    Post.get = function(username, callback) {
+    //读取post集合,获取所有文章
+    Post.getAll = function(name, callback) {
         //打开数据库
         mongodb.open(function (err, db) {
             if (err) {
@@ -55,8 +59,8 @@ Post.prototype.save=function (callback) {
                     return callback(err);
                 }
                 var query = {};
-                if (username) {
-                    query.username = username;
+                if (name) {
+                    query.name = name;
                 }
                 //根据 query 对象查询文章
                 collection.find(query).sort({
@@ -66,12 +70,123 @@ Post.prototype.save=function (callback) {
                     if (err) {
                         return callback(err);//失败！返回 err
                     }
-                    callback(null, docs);//成功！以数组形式返回查询的结果
+                    //将每篇文章在读取的时候以markdown的格式进行解析
+                    docs.forEach(function (doc) {
+                        doc.content=markdown.toHTML(doc.content);
+                    })
+                   return callback(null, docs);//成功！以数组形式返回查询的结果
                 });
             });
         });
-    //插入数据，并跳转到首页
 
 }
-
+   //获取一篇文章
+   Post.getOne=function (name,title,time,callback) {
+       mongodb.open(function (err,db) {
+           if(err){
+               return callback(err);
+           }
+           db.collection('posts',function (err,collection) {
+               if(err){
+                   mongodb.close();
+                   return callback(err);
+               }
+               collection.findOne({
+                   name:name,
+                   title:title,
+                   time:time
+               },function (err,doc) {
+                   mongodb.close();
+                   if(err){
+                       return callback(err);
+                   }
+                  console.log(doc);
+                   if(doc){
+                       //markdown解析文章的内容
+                       doc.content = markdown.toHTML(doc.content);
+                       //留言的内容也要通过markdown来进行解析
+                       doc.comments.forEach(function(comment){
+                           comment.c_content = markdown.toHTML(comment.c_content)
+                       })
+                   }
+                   return callback(null,doc);
+               })
+           })
+       })
+   }
+   //编辑逻辑
+  Post.edit=function (name,title,time,callback) {
+      mongodb.open(function (err, db) {
+          if (err) {
+              return callback(err);
+          }
+          db.collection('posts', function (err, collection) {
+              if (err) {
+                  mongodb.close();
+                  return callback(err);
+              }
+              collection.findOne({
+                  name: name,
+                  title: title,
+                  time: time
+              }, function (err, doc) {
+                  mongodb.close();
+                  if (err) {
+                      return callback(err);
+                  }
+                  return callback(null, doc);
+              })
+          })
+      })
+  }
+      //编辑
+  Post.update=function (name, title, time,content, callback) {
+        mongodb.open(function (err,db) {
+            if(err){
+                return callback(err);
+            }
+            db.collection('posts',function (err,collection) {
+                if(err){
+                    mongodb.close();
+                    return callback(err);
+                }
+                collection.update({
+                    name:name,
+                    title:title,
+                    time:time
+                },{$set:{content:content}},function (err,doc) {
+                    mongodb.close();
+                    if(err){
+                        return callback(err);
+                    }
+                    return callback(null,doc);
+                })
+            })
+        })
+      }
+      //删除
+  Post.remove=function (name,title,time,callback) {
+         mongodb.open(function (err,db) {
+             if(err){
+                 return callback(err);
+             }
+             db.collection('posts',function (err,collection) {
+                 if(err){
+                     mongodb.close();
+                     return callback(err);
+                 }
+                 collection.remove({
+                     name:name,
+                     title:title,
+                     time:time
+                 },{w:1},function (err) {
+                     mongodb.close();
+                     if(err){
+                         return callback(err);
+                     }
+                    return callback(null);
+                 })
+             })
+         })
+     }
 module.exports=Post;
